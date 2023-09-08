e073214d7c3a8f09acc1803d46483cc8d350e587
박승호 <vatzz@devswift.me>
2023-09-08 20:05:06 +0900

    Initial Commit

---
+++ Speech-To-Text/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speech-To-Text
+//
+//  Created by 박승호 on 2023/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Speech-To-TextUITests/Speech_To_TextUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Speech_To_TextUITestsLaunchTests.swift
+//  Speech-To-TextUITests
+//
+//  Created by 박승호 on 2023/09/08.
+//
+
+import XCTest
+
+final class Speech_To_TextUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
