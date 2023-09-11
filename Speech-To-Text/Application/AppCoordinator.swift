faed79bf4086592d4141370a5dfb54aa6581201e
박승호 <p_vatzz@naver.com>
2023-09-12 05:11:45 +0900

    Coordinator 생성 중.

--- Speech-To-Text/Application/AppCoordinator.swift
+++ Speech-To-Text/Application/AppCoordinator.swift
@@ -8,6 +8,31 @@
 import Foundation
 import UIKit
 
+protocol Coordinator {
+    
+    func transition(scene: Scene, style: TransitionType, isAnimated: Bool)
+    func close(isAnimated: Bool)
+}
+
+extension Coordinator {
+    
+    func transition(scene: Scene, style: TransitionType, isAnimated: Bool) {
+        let vc = scene.instantiate()
+        
+        switch style {
+        case .root: break
+        
+        case .push: break
+            
+        case .modal: break
+        }
+    }
+    
+    func close(isAnimated: Bool) {
+        
+    }
+}
+
 class AppCoordinator {
     let navController: UINavigationController
     let diContainer: AppDIContainer
