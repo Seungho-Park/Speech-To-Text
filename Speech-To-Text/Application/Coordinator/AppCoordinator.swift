69c9312fb0a0b80df7b3b3745ff772251211be37
박승호 <p_vatzz@naver.com>
2023-09-12 07:22:43 +0900

    commit

---
+++ Speech-To-Text/Application/Coordinator/AppCoordinator.swift
@@ -0,0 +1,30 @@
+//
+//  AppCoordinator.swift
+//  Speech-To-Text
+//
+//  Created by 박승호 on 2023/09/09.
+//
+
+import Foundation
+import UIKit
+
+private extension UIViewController {
+    var sceneViewController: UIViewController {
+        self.children.first ?? self
+    }
+}
+
+class AppCoordinator: Coordinator {
+    let window: UIWindow
+    var currentScene: UIViewController? = nil
+    let diContainer: AppDIContainer
+    
+    init(window: UIWindow, diContainer: AppDIContainer) {
+        self.window = window
+        self.diContainer = diContainer
+    }
+    
+    func start() {
+        
+    }
+}
--- Speech-To-Text/Application/Coordinator/Coordinator.swift
+++ Speech-To-Text/Application/Coordinator/Coordinator.swift
@@ -1,8 +1,8 @@
 //
-//  AppCoordinator.swift
+//  Coordinator.swift
 //  Speech-To-Text
 //
-//  Created by 박승호 on 2023/09/09.
+//  Created by 박승호 on 2023/09/12.
 //
 
 import Foundation
@@ -52,26 +52,5 @@ extension Coordinator {
         } else {
             //TODO: Error -
         }
-    }
-}
-
-extension UIViewController {
-    var sceneViewController: UIViewController {
-        self.children.first ?? self
-    }
-}
-
-class AppCoordinator: Coordinator {
-    let window: UIWindow
-    var currentScene: UIViewController? = nil
-    let diContainer: AppDIContainer
-    
-    init(window: UIWindow, diContainer: AppDIContainer) {
-        self.window = window
-        self.diContainer = diContainer
-    }
-    
-    func start() {
-        
     }
 }
